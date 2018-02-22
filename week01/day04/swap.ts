d70a40bc24987e58eef52e8c298914a1065d33f5
Dóra László <design.doralaszlo@gmail.com>
2018-02-22 12:22:23 +0100

    done

---
+++ week01/day04/swap.ts
@@ -0,0 +1,14 @@
+'use strict';
+
+// Swap the values of these variables
+let a: number = 123;
+let b: number = 526;
+let temp: number;
+
+temp = a;
+a = b;
+b = temp;
+
+console.log(a);
+console.log(b);
+
