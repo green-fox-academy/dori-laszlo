d83bcb063c7f68f0491e128485aeef67fe81f262
Dóra László <design.doralaszlo@gmail.com>
2018-02-25 15:49:00 +0100

    done

---
+++ week02/day01/append-a.ts
@@ -0,0 +1,18 @@
+'Use strict';
+
+// -  Create an array variable named `nimals`
+//    with the following content: `["kuty", "macsk", "cic"]`
+// -  Add all elements an `"a"` at the end
+
+let nimals: string[] = ['kuty', 'macsk', 'cic'];
+
+function appenda (animals: string[]) {
+    for(let i: number = 0; i < animals.length; i++) {
+        animals[i] += 'a';
+        console.log(animals[i]);
+    }
+}
+
+appenda(nimals);
+
+
