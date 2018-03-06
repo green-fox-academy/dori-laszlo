a74de4910feefdc53c693a52a4057f716f08d6ce
Dóra László <design.doralaszlo@gmail.com>
2018-03-06 14:18:05 +0100

    done

---
+++ week04/day02/homework.ts
@@ -0,0 +1,116 @@
+'Use strict';
+
+class Person {
+    public name: string;
+    public age: number;
+    public gender: string;
+
+    constructor(name: string = 'Jane Doe', age: number = 30, gender: string = 'female') {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    }
+
+    introduce() {
+        console.log(`Hi, I\'m ${this.name}, a ${this.age} year old ${this.gender}.`);
+    }
+
+    getGoal() {
+        console.log('My goal is: Live for the moment!');
+    }
+}
+
+class Student extends Person {
+    public previousOrganization: string;
+    public skippedDays: number;
+
+    constructor(name?: string, age?: number, gender?: string, previousOrganization: string = 'School of Life', skippedDays: number = 0) {
+        super(name, age, gender);
+        this.previousOrganization = previousOrganization;
+        this.skippedDays = 0;
+    }
+    
+    getGoal() {
+        console.log('My goal is: Be a junior software developer.');
+    }
+
+    introduce() {
+        console.log(`Hi, I\'m ${this.name}, a ${this.age} year old ${this.gender} from ${this.previousOrganization} who skipped ${this.skippedDays} days from the course already."`);
+    }
+
+    skipDays(numberOfDays) {
+        return this.skippedDays += numberOfDays;
+    }
+}
+
+class Mentor extends Person {
+    public level: string;
+    constructor(name?: string, age?: number, gender?: string, level: string = 'intermediate') {
+        super(name, age, gender);
+        this.level = level;
+    }
+
+    getGoal() {
+        console.log('My goal is: Educate brilliant junior software developers.');
+    }
+
+    introduce() {
+        console.log(`Hi, I\'m ${this.name}, a ${this.age} year old ${this.gender} ${this.level} mentor.`);
+    }
+
+}
+
+class Sponsor extends Person {
+    public company: string;
+    public hiredStudents: number;
+
+    constructor(name?: string, age?: number, gender?: string, company?: string, hiredStudents: number = 0) {
+        super(name, age, gender);
+        this.company = company;
+        this.hiredStudents = hiredStudents;
+    }
+
+    introduce() {
+        console.log(`Hi, I\'m ${this.name}, a ${this.age} year old ${this.gender}, who represents ${this.company} and hired ${this.hiredStudents} students so far`);
+    }
+
+    getGoal() {
+        console.log('My goal is: Hire brilliant junior software developers.');
+    }
+
+    hire() {
+        return this.hiredStudents++;
+    }
+}
+
+let people = [];
+let mark = new Person('Mark', 46, 'male');
+people.push(mark);
+let jane = new Person();
+people.push(jane);
+let john = new Student('John Doe', 20, 'male', 'BME');
+people.push(john);
+let student = new Student();
+people.push(student);
+let gandhi = new Mentor('Gandhi', 148, 'male', 'senior');
+people.push(gandhi);
+let mentor = new Mentor();
+people.push(mentor);
+let sponsor = new Sponsor();
+let elon = new Sponsor('Elon Musk', 46, 'male', 'SpaceX');
+people.push(elon);
+
+student.skipDays(3);
+
+for (let i = 0; i < 6; i++) {
+  elon.hire();
+}
+
+for (let i = 0; i < 4; i++) {
+  console.log(sponsor.hire());
+}
+
+for (let person of people) {
+  console.log(person.introduce());
+  console.log(person.getGoal());
+}
