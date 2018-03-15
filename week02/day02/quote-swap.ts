7eaea87e79c813087df0943dd1a6696ad7fcc3ac
Dóra László <design.doralaszlo@gmail.com>
2018-03-15 01:17:11 +0100

    unnecessary object deleted

--- week02/day02/quote-swap.ts
+++ week02/day02/quote-swap.ts
@@ -5,16 +5,6 @@
 // Also, log the sentence to the console with spaces in between.
 // Create a function called quoteSwap().
 let words: string[] = ['What', 'I', 'do', 'create,', 'I', 'cannot', 'not', 'understand.'];
-let keyNumbers = {
-    0: 0,
-    1: 1,
-    2: 5,
-    3: 3,
-    4: 4,
-    5: 2,
-    6: 6,
-    7: 7,
-};
 
 function quoteSwap(mixedWords) {
     mixedWords[5] = mixedWords.splice(2, 1, mixedWords[5]);
@@ -22,7 +12,6 @@ function quoteSwap(mixedWords) {
 };
 
 console.log(quoteSwap(words));
-
 
 // Expected output: "What I cannot create I do not understand."
 export = quoteSwap;
