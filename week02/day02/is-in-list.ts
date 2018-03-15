be19b1d72896dbc08158ee4971d3e4d04f57d655
Dóra László <design.doralaszlo@gmail.com>
2018-03-16 00:20:17 +0100

    done

--- week02/day02/is-in-list.ts
+++ week02/day02/is-in-list.ts
@@ -3,6 +3,15 @@
 // Create a function that accepts 'listOfNumbers' as an input
 // it should return "true" if it contains all, otherwise "false"
 let listOfNumbers: number[] = [2, 4, 6, 8, 10, 12, 14, 16];
+
+function checkNums(listOfNumbers) {
+    if (listOfNumbers.indexOf(4 && 8 && 12 && 16) != -1) {
+        return true;
+    } else {
+        return false;
+    }
+};
+
 console.log(checkNums(listOfNumbers));
 
-//export = checkNums;
+export = checkNums;
