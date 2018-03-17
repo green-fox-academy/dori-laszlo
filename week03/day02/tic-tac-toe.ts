346e970ad010d58187cc6ac3c88fdbd9c54d9ace
Dóra László <design.doralaszlo@gmail.com>
2018-03-17 19:27:11 +0100

    done

--- week03/day02/tic-tac-toe.ts
+++ week03/day02/tic-tac-toe.ts
@@ -1,22 +1,27 @@
-'Use strict';
+'use strict';
 
 // Write a function that takes a filename as string,
 // open and read it. The file data represents a tic-tac-toe
 // game result. Return 'X'/'O'/'draw' based on which player
 // has winning situation.
+
+const fs = require('fs');
+
+function ticTacResult(filename: string) {
+    let read = fs.readFileSync(filename, 'utf-8');
+    if (read === 'x') {
+        return 'X';
+    } if (read === 'o') {
+        return 'O';
+    } if (read === 'draw') {
+        return 'draw';
+    }
+};
+
+
 console.log(ticTacResult('win-o.txt'))
 // should print O
 console.log(ticTacResult('win-x.txt'))
 // should print X
 console.log(ticTacResult('draw.txt'))
 // should print draw
-
-const fs = require('fs');
-
-function ticTacResult(fileName: string) {
-    fs.readFileSync(fileName, 'utf-8');
-    if (fileName === O) {
-    return console.log(0); 
-    }
-
-}
