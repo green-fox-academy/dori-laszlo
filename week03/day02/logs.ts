a5866e67b70c96fdcd5c04be23208c12493e6bbf
Dóra László <design.doralaszlo@gmail.com>
2018-03-17 19:36:34 +0100

    undone

---
+++ week03/day02/logs.ts
@@ -0,0 +1,11 @@
+'use strict';
+
+// Read all data from 'log.txt'.
+// Each line represents a log message from a web server
+// Write a function that returns an array with the unique IP adresses.
+// Write a function that returns the GET / POST request ratio.
+
+const fs = require('fs');
+
+fs.readFileSync('log.txt', 'utf-8');
+
