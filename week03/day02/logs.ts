b4ec3ca30a16847e79b8bad0d52faede3c914955
Dóra László <design.doralaszlo@gmail.com>
2018-03-22 11:07:03 +0100

    just unique ip addresses

--- week03/day02/logs.ts
+++ week03/day02/logs.ts
@@ -7,5 +7,38 @@
 
 const fs = require('fs');
 
-fs.readFileSync('log.txt', 'utf-8');
+function ipAddresses(file: string) {
+  let rows = fs.readFileSync(file, 'utf-8').split('\n');
+  let elements = [];
+  let ipAddresses = [];
+  for (let i: number = 0; i < rows.length; i++) {
+    elements.push(rows[i].split(' '));
+  }
+  for (let i: number = 0; i < elements.length; i++) {
+    if (ipAddresses.indexOf(elements[i]) === -1) {
+      ipAddresses.push(elements[i][8]);
+    }
+  }
+  return ipAddresses;
+};
 
+function ratioGetPos(file: string) {
+  let rows = fs.readFileSync(file, 'utf-8').split('\n');
+  let elements = [];
+  let POST = 0;
+  let GET = 0;
+  for (let i: number = 0; i < rows.length; i++) {
+    elements.push(rows[i].split(' '));
+  }
+  for (let i: number = 0; i < elements.length; i++) {
+    if (elements[i][11] === 'POST') {
+      POST++;
+    } if (elements[i][11] === 'GET') {
+      GET++;
+    }
+  }
+  return `GET: ${GET} / POST: ${POST}`;
+};
+
+console.log(ipAddresses('log.txt'));
+console.log(ratioGetPos('log.txt'));
