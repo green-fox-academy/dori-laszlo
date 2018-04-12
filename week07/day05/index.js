26bc4c5cbf5149a0a0e0e9e99c80b87510267c64
Dóra László <design.doralaszlo@gmail.com>
2018-04-12 20:30:42 +0200

    backend OK

---
+++ week07/day05/index.js
@@ -0,0 +1,140 @@
+let mysql = require('mysql');
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+
+const app = express();
+const PORT = 3000;
+
+
+let conn = mysql.createConnection ({
+  host: 'localhost',
+  user: 'root',
+  password: 'a',
+  database: 'reddit',
+});
+
+conn.connect(function(err) {
+  if (err) {
+    console.log('Error connecting to Db');
+    return;
+  }
+  console.log('Connection established');
+});
+
+// conn.end(
+
+app.get('/', (req, res) => {
+  conn.query('SELECT * FROM posts;', function(err, rows) {
+    if (err) {
+      console.log(err.toString());
+      res.satus(500).send('Database error');
+      return;
+    }
+    console.log(rows);
+    res.render('home', {
+      posts: rows,
+      title: 'Posts',
+    })
+  });
+});
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.json());
+app.use('/static', express.static(path.join(__dirname, 'static')));
+
+app.get('/hello', (req, res) => {
+  res.json({
+    hello: 'Hello World!',
+    title: 'Hello',
+  });
+});
+
+app.get('/posts', function(req, res) {
+  conn.query('SELECT * FROM posts;', function(err, rows) {
+    if (err) {
+      console.log(err.toString());
+      res.satus(500).send('Database error');
+      return;
+    }
+    console.log(rows);
+    res.render('home', {
+      posts: rows,
+      title: 'Posts',
+    })
+  });
+});
+
+app.get('/api/posts', function(req, res) {
+  conn.query('SELECT * FROM posts;', function(err, rows) {
+    if (err) {
+      console.log(err.toString());
+      res.satus(500).send('Database error');
+      return;
+    }
+    res.json(rows);
+  });
+});
+
+app.post('/posts', (req, res) => {
+  conn.query(`INSERT INTO posts (title, url, score) VALUES ('${req.body.title}', '${req.body.url}', 1);`, function(err, rows) {
+    if (err) {
+      console.log(err.toString());
+      res.status(500).send('Database error');
+      return;
+    }
+    res.json({
+      message: 'OK',
+      id: rows.insertId,
+      title: 'Posts',
+    });
+  });
+});
+
+app.put('/posts/:id/upvote', (req, res) => {
+  conn.query(`UPDATE posts SET score = score + 1 WHERE id = ${req.params.id}`, function(err, rows) {
+    if (err) {
+      console.log(err.toString());
+      res.status(500).send('Database error');
+      return;
+    }
+    res.json({
+      message: 'OK',
+      title: 'Posts',
+    });
+  });
+});
+
+app.put('/posts/:id/downvote', (req, res) => {
+  conn.query(`UPDATE posts SET score = score - 1 WHERE id = ${req.params.id}`, function(err, rows) {
+    if (err) {
+      console.log(err.toString());
+      res.status(500).send('Database error');
+      return;
+    }
+    res.json({
+      message: 'OK',
+      title: 'Posts',
+    });
+  });
+});
+
+app.delete('/posts/:id', (req, res) => {
+  conn.query(`DELETE FROM posts WHERE id = ${req.params.id}`, function(err, rows) {
+    if (err) {
+      console.log(err.toString());
+      res.status(500).send('Database error');
+      return;
+    }
+    res.json({
+      message: 'OK',
+      title: 'Posts',
+      id: req.params.id,
+    });
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is up on port ${PORT}`);
+});
