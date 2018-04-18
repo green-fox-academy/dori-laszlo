b72f7a86145e6898a463c2cae6ae41895855d7a9
Dóra László <design.doralaszlo@gmail.com>
2018-04-18 11:38:10 +0200

    test case corrected

--- week09/day02/iamgroot/routes.js
+++ week09/day02/iamgroot/routes.js
@@ -4,12 +4,12 @@ const app = express();
 app.get('/groot/', (req, res) => {
   // TODO: implement this method
   if (req.query.message === undefined) {
-    res.json({
+    res.status(400).json({
       error: 'I am Groot!'
     })
   } else {
     res.status(200).json({
-      message: req.query,
+      message: req.query.message,
       translated: 'I am Groot!',
     });
   }
