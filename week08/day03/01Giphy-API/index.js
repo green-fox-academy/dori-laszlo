97230ff2e4a36ca75bdc85ed9381446b70517f74
Dóra László <design.doralaszlo@gmail.com>
2018-05-01 17:02:50 +0200

    corrected with boolean switch

--- week08/day03/01Giphy-API/index.js
+++ week08/day03/01Giphy-API/index.js
@@ -6,21 +6,20 @@ xhr.onload = () => {
   let img = document.createElement('img');
   let dataFromGiphy = JSON.parse(xhr.responseText);
   console.log(dataFromGiphy);
-  // img.setAttribute('src', JSON.parse(xhr.responseText).data[0].images.original.url);
-  // gif.appendChild(img);
-  let clickCounter = 0;
+  
   for (let i = 0; i < dataFromGiphy.data.length; i++) {
     let thumbnail = document.createElement('img');
+    let isMoving = false;
     thumbnail.addEventListener('click', function () {
-      if (clickCounter === 0) {
+      if (isMoving === false) {
         thumbnail.setAttribute('src', JSON.parse(xhr.responseText).data[i].images.fixed_height.url);
-        clickCounter++;
+        isMoving = true;
       } else {
-        thumbnail.setAttribute('src', JSON.parse(xhr.responseText).data[i].images.fixed_height_small_still.url);
-        clickCounter = 0;
+        thumbnail.setAttribute('src', JSON.parse(xhr.responseText).data[i].images.fixed_height_still.url);
+        isMoving = false;
       }
     });
-    thumbnail.setAttribute('src', JSON.parse(xhr.responseText).data[i].images.fixed_height_small_still.url);
+    thumbnail.setAttribute('src', JSON.parse(xhr.responseText).data[i].images.fixed_height_still.url);
     gif.appendChild(thumbnail);
   }
 }
